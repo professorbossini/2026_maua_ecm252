b1cc27e33b0a02ef1be1076db8ad023b1d753e47
Rodrigo Bossini <professorbossini@gmail.com>
2026-05-09 12:11:56 -0300

    learn(ap007): encerra pg 21
    - retomar atualizando a variavel de estado de fotos

--- ap007-react-exibe-fotos/src/components/App.jsx
+++ ap007-react-exibe-fotos/src/components/App.jsx
@@ -5,21 +5,39 @@ import 'primeicons/primeicons.css'
 import 'primereact/resources/primereact.min.css'
 import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
 import Busca from './Busca'
-const App = () => {
-  return (
-    <div className='grid justify-content-center m-auto w-9 border-round border-1'>
+import { createClient } from 'pexels'
+export default class App extends React.Component{
 
-      <div className='col-12'>
-        <i className="pi pi-apple"></i>
+  pexelsClient = null
+
+  onBuscaRealizada = (termoDeBusca) => {
+    this.pexelsClient.photos.search({
+      query: termoDeBusca
+    })
+    .then((result => console.log(result)))
+  }
+
+  componentDidMount(){
+    this.pexelsClient = createClient('a91Qyfh2Ud1rdeOGKV8aTR5Aj9UmRvdma6EdyhC9EfKStoAyt7rmDuhV')
+  }
+
+  render(){
+    return (
+      <div className='grid justify-content-center m-auto w-9 border-round border-1'>
+  
+        <div className='col-12'>
+          <i className="pi pi-apple"></i>
+        </div>
+        <div className='col-12'>
+          <h1>Exibir uma lista de...</h1>
+        </div>
+        <div className="col-12">
+          <Busca 
+            dica="Digite algo que deseja ver..."
+            onBuscaRealizada={this.onBuscaRealizada} />
+        </div>
       </div>
-      <div className='col-12'>
-        <h1>Exibir uma lista de...</h1>
-      </div>
-      <div className="col-12">
-        <Busca dica="Digite algo que deseja ver..." />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default App
